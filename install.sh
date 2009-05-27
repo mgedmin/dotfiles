e49535f4bd14d245c7ed773b568fc69c6ea08762
mg <mg@4fc293c4-4eed-0310-a01a-b4ad72f90fad>
2009-05-27 22:25:39 +0000

    Only try to symlink what doesn't exist.
    git-svn-id: svn+ssh://fridge.pov.lt/home/mg/svn/mg-home/dotfiles@1702 4fc293c4-4eed-0310-a01a-b4ad72f90fad

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 # assumes this directory is ~/dotfiles
-cd ~ && ln -s dotfiles/.[a-z]* ~/
+for fn in .[a-z]*; do
+    if [ "$fn" = ".svn" ]; then
+        continue # don't fiddle with svn metadata!!!
+    fi
+    if [ -L "$HOME/$fn" ] && [ "`readlink $HOME/$fn`" = "dotfiles/$fn" ]; then
+        continue # already a symlink to the right place
+    fi
+    ln -s dotfiles/$fn $HOME/
+done
