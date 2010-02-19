26c1070e1057734519a02b479debbb7824bef19c
mg <mg@4fc293c4-4eed-0310-a01a-b4ad72f90fad>
2010-02-19 11:23:08 +0000

    New script to grab a new dotfile.
    git-svn-id: svn+ssh://fridge.pov.lt/home/mg/svn/mg-home/dotfiles@1766 4fc293c4-4eed-0310-a01a-b4ad72f90fad

---
+++ grab.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# assumes this directory is ~/dotfiles
+rc=0
+for arg; do
+    fn=${arg#.}
+    if ! [ x".$fn" = x"$arg" ]; then
+        echo "$arg: not a dotfile" 1>&2
+        rc=1
+        continue
+    fi
+    if [ -L "$HOME/.$fn" ] && [ "`readlink $HOME/.$fn`" = "dotfiles/$fn" ]; then
+        continue # already a symlink to the right place
+    fi
+    if [ -e "$HOME/dotfiles/$fn" ]; then
+        echo "$arg: ~/dotfiles/$fn already exists" 1>&2
+        rc=1
+        continue
+    fi
+    mv -i $HOME/.$fn $HOME/dotfiles/$fn
+    ln -s dotfiles/$fn $HOME/.$fn
+done
+exit $rc
