ad3a9b4b3eb5521b3f4ccb7708987ee894f4ee39
Marius Gedminas <marius@gedmin.as>
2015-09-17 12:58:16 +0300

    Add ungrab.sh

---
+++ ungrab.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# assumes this directory is ~/dotfiles
+cd ~/dotfiles/
+rc=0
+for arg; do
+    s=${arg#$HOME/}
+    fn=${s#.}
+    if ! [ x".$fn" = x"$s" ]; then
+        echo "$arg: not a dotfile" 1>&2
+        rc=1
+        continue
+    fi
+    if [ -L "$HOME/.$fn" ] && [ "$(readlink "$HOME/.$fn")" = "dotfiles/$fn" ]; then
+        # a symlink to the right place
+        rm "$HOME/.$fn"
+        cp "$fn" "$HOME/.$fn"
+    else:
+        echo "$arg: not a symlink to ~/dotfiles" 1>&2
+        rc=1
+        continue
+    fi
+done
+exit $rc
