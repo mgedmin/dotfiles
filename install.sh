7f72ba613fbdcf07d3795df7b9fa072428648fdf
Marius Gedminas <marius@gedmin.as>
2016-09-10 17:09:42 +0300

    Hostname-specific local dotfiles
    Also refactor code to be less repetetive.
    
    Also add command-line parsing and -v (verbose) mode.
    
    Also drop fixup for ancient scheme (.foo -> dotfiles/.foo instead of
    dotfiles/foo).

--- install.sh
+++ install.sh
@@ -1,34 +1,59 @@
 #!/bin/bash
 # assumes this directory is ~/dotfiles
-pushd ~/dotfiles/ > /dev/null
+
+verbose=0
+while getopts v OPT; do
+    case "$OPT" in
+        v)
+            verbose=$((verbose + 1))
+            ;;
+        *)
+            printf "%s: unknown option %s\n" "$0" "$OPT" 1>&2
+            exit 1
+            ;;
+    esac
+done
+
+emit() { local lvl=$1; shift; [ $verbose -ge $lvl ] && printf "%s\n" "$*"; }
+info() { emit 1 "$*"; }
+debug() { emit 2 "$*"; }
+
+pushd ~/dotfiles/ > /dev/null || exit 1
 for fn in [a-z]*; do
+    dotfile=$HOME/.$fn
+    target=dotfiles/$fn
+    skeleton=/etc/skel/.$fn
     case "$fn" in
-        *.sh)
+        *.local.$HOSTNAME)
+            dotfile=$HOME/.${fn%.$HOSTNAME}
+            ;;
+        *.sh|*.local.*)
+            debug "skipping $fn"
             continue # ignore
             ;;
         *)
             ;;
     esac
-    if [ -L "$HOME/.$fn" ] && [ "`readlink $HOME/.$fn`" = "dotfiles/$fn" ]; then
+    if [ -L "$dotfile" ] && [ "$(readlink $dotfile)" = "$target" ]; then
+        debug "already exists: $dotfile -> $target"
         continue # already a symlink to the right place
     fi
-    if [ -L "$HOME/.$fn" ] && [ "`readlink $HOME/.$fn`" = "dotfiles/.$fn" ]; then
-        # move old symlink to new one
-        rm $HOME/.$fn
-    fi
-    if [ -f "$HOME/.$fn" ]; then
-        if cmp "$HOME/.$fn" "$HOME/dotfiles/$fn" > /dev/null; then
-            echo "contents identical, replacing $HOME/.$fn with symlink"
-            rm $HOME/.$fn
-        elif [ -f "/etc/skel/.$fn" ] && cmp "$HOME/.$fn" "/etc/skel/.$fn" > /dev/null; then
-            echo "identical to /etc/skel/ version, replacing $HOME/.$fn with symlink"
-            rm $HOME/.$fn
+    if [ -f "$dotfile" ]; then
+        if cmp "$dotfile" "$HOME/$targets" > /dev/null; then
+            echo "contents identical, replacing $dotfile with symlink"
+            rm $dotfile
+        elif [ -f "$skeleton" ] && cmp "$dotfile" "$skeleton" > /dev/null; then
+            echo "identical to /etc/skel/ version, replacing $dotfile with symlink"
+            rm $dotfile
         else
-            echo "contents differ, skipping: $HOME/.$fn $HOME/dotfiles/$fn" 1>&2
+            # print both files with a space so I can easily copy them
+            # together and paste as arguments to vimdiff
+            echo "contents differ, skipping: $dotfile $HOME/$target" 1>&2
             continue
         fi
     fi
-    ln -s dotfiles/$fn $HOME/.$fn
+    info "$dotfile -> $target"
+    ln -s $target $dotfile
 done
 if [ -n "$BASH_VERSION" ] && [ -n "$PS1" ]; then
     # this looks like an interactive session, i.e. the user did
