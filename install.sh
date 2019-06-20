075813220152dfcf3bc5ae6536edc77e0306fe82
Marius Gedminas <marius@gedmin.as>
2019-06-20 15:52:28 +0300

    Support dot subdirectories

--- install.sh
+++ install.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 # assumes this directory is ~/dotfiles
 
+usage="usage: $0 [-n] [-v]
+options:
+  -n    don't make any changes
+  -v    be more verbose (repeat for extra verbosity)
+"
+
 verbose=0
-while getopts v OPT; do
+dry_run=0
+while getopts hvn OPT; do
     case "$OPT" in
+        h)
+            printf "%s" "$usage"
+            exit 0
+            ;;
+        n)
+            dry_run=1
+            ;;
         v)
             verbose=$((verbose + 1))
             ;;
@@ -18,54 +32,89 @@ emit() { local lvl=$1; shift; [ $verbose -ge "$lvl" ] && printf '%s\n' "$*"; }
 info() { emit 1 "$*"; }
 debug() { emit 2 "$*"; }
 
-pushd ~/dotfiles/ > /dev/null || exit 1
-for fn in [a-z]*; do
-    dotfile=$HOME/.$fn
-    target=dotfiles/$fn
-    skeleton=/etc/skel/.$fn
-    case "$fn" in
-        *.local.$HOSTNAME|mailcheckrc.$HOSTNAME)
-            dotfile=$HOME/.${fn%.$HOSTNAME}
-            ;;
-        *.sh|*.local.*|mailcheckrc.*|core|core.*)
-            debug "skipping $fn"
-            continue # ignore
-            ;;
-        *)
-            ;;
-    esac
+run() {
+    if [ $dry_run -eq 0 ]; then
+        "$@"
+    else
+        printf '%s\n' "$*"
+    fi
+}
+
+process() {
+    local dotfile=$1
+    local target=$2
+    local skeleton=$3
+    local dir
+    dir=$(dirname "$dotfile")
+
     if [ -L "$dotfile" ] && [ "$(readlink "$dotfile")" = "$target" ]; then
         debug "already exists: $dotfile -> $target"
-        continue # already a symlink to the right place
+        return # already a symlink to the right place
     fi
     if [ -f "$dotfile" ]; then
         if cmp "$dotfile" "$HOME/$target" > /dev/null; then
             echo "contents identical, replacing $dotfile with symlink"
-            rm "$dotfile"
+            run rm "$dotfile"
         elif [ -f "$skeleton" ] && cmp "$dotfile" "$skeleton" > /dev/null; then
             echo "identical to /etc/skel/ version, replacing $dotfile with symlink"
-            rm "$dotfile"
+            run rm "$dotfile"
         else
             # print both files with a space so I can easily copy them
             # together and paste as arguments to vimdiff
             echo "contents differ, skipping: $dotfile $HOME/$target" 1>&2
-            continue
+            return
         fi
     else
         info "$dotfile -> $target"
     fi
-    ln -s "$target" "$dotfile"
-done
-if [ -n "$BASH_VERSION" ] && [ -n "$PS1" ]; then
-    # this looks like an interactive session, i.e. the user did
-    #   . ~/dotfiles/install.sh
-    # instead of
-    #   ~/dotfiles/install.sh
-    # so let's reload their .profile and .inputrc for convenience
-    echo "reloading .profile"
-    # shellcheck source=/dev/null
-    . ~/.profile
-    echo "reloading .inputrc"
-    bind -f ~/.inputrc
-fi
-popd > /dev/null || exit 1
+    if ! [ -d "$dir" ]; then
+        run mkdir -p "$dir"
+    fi
+    run ln -sr "$HOME/$target" "$dotfile"
+}
+
+process_dir() {
+    local prefix="$1"
+    local fn
+    for fn in "$prefix"[a-z]*; do
+        if [ -d "$fn" ]; then
+            process_dir "$fn/"
+        else
+            local dotfile=$HOME/.$fn
+            local target=dotfiles/$fn
+            local skeleton=/etc/skel/.$fn
+            case "$fn" in
+                *.local.$HOSTNAME|mailcheckrc.$HOSTNAME)
+                    dotfile=$HOME/.${fn%.$HOSTNAME}
+                    ;;
+                *.sh|*.local.*|mailcheckrc.*|core|core.*)
+                    debug "skipping $fn"
+                    continue # ignore
+                    ;;
+                *)
+                    ;;
+            esac
+            process "$dotfile" "$target" "$skeleton"
+        fi
+    done
+}
+
+main() {
+    pushd ~/dotfiles/ > /dev/null || exit 1
+    process_dir ""
+    if [ -n "$BASH_VERSION" ] && [ -n "$PS1" ] && [ $dry_run -eq 0 ]; then
+        # this looks like an interactive session, i.e. the user did
+        #   . ~/dotfiles/install.sh
+        # instead of
+        #   ~/dotfiles/install.sh
+        # so let's reload their .profile and .inputrc for convenience
+        echo "reloading .profile"
+        # shellcheck source=/dev/null
+        . ~/.profile
+        echo "reloading .inputrc"
+        bind -f ~/.inputrc
+    fi
+    popd > /dev/null || exit 1
+}
+
+main
