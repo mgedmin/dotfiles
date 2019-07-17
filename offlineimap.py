8bac7ba024075b8d1fa0244b78e9358b5ba310bd
Marius Gedminas <marius@gedmin.as>
2019-07-17 21:40:45 +0300

    Fix offlineimap folder sorting

--- offlineimap.py
+++ offlineimap.py
@@ -1,16 +1,18 @@
 # see offlineimap(1)
 #
-priority = {'IN.personal': -10,
-            'INBOX': -10,
-            'IN.root': -9,
-            'postponed': -7,
-            'mbox': -1,
-            'sent-mail': -1,
-            'IN.b4net': 5,
-            'SPAM': 10,
-            'IN.SPAM': 10,
-            'IN.virus': 10,
-           }
+priority = {
+    'IN.personal': -10,
+    'INBOX': -10,
+    'IN.root': -9,
+    'postponed': -7,
+    'mbox': -1,
+    'sent-mail': -1,
+    'IN.b4net': 5,
+    'SPAM': 10,
+    'IN.SPAM': 10,
+    'IN.virus': 10,
+}
+
 
 def prioritize(x):
     # old offlineimap: x is a folder name
@@ -20,9 +22,8 @@ def prioritize(x):
         default = -5
     else:
         default = 0
-    return priority.get(name, default)
+    return (priority.get(name.replace('/', '.'), default), name)
+
 
 def mycmp(x, y):
-    return cmp((prioritize(x), x),
-               (prioritize(y), y))
-
+    return cmp(prioritize(x), prioritize(y))
